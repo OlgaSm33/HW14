0a266e2a522ba80a5bd4a38bed312a9bd730217c
OlgaSm33 <olya.sudakova98@gmail.com>
2025-02-16 20:47:46 +0700

    Добавьте файлы проекта.

---
+++ HW14/PhoneBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    public class PhoneBook
+    {
+        public List<Contact> _contacts {  get; set; }
+
+
+
+        public PhoneBook(List<Contact> contacts)
+        {
+            _contacts = contacts;
+
+            
+        }
+
+        public PhoneBook() 
+        {
+            _contacts = new List<Contact>();
+
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+            _contacts = _contacts.OrderBy(contact => contact.Name).ThenBy(contact => contact.LastName).ToList();
+        }
+
+        public void ContactsShowByPages (int pageNumber, int contactsOnPageCount = 2)
+        {
+            int contactsCount = _contacts.Count();
+            int pagesCount = Convert.ToInt32(Math.Round(contactsCount / (double)contactsOnPageCount, MidpointRounding.AwayFromZero));
+            IEnumerable<Contact> page;
+            switch (pageNumber)
+            {
+                case int n when (n >= 1 && n <= pagesCount):
+
+                    int skipCount = (n - 1) * contactsOnPageCount;
+                    page = _contacts.Skip(skipCount).Take(contactsOnPageCount);
+                    break;
+                default:
+                    page = null;
+                    break;
+            }
+            if (page == null)
+            {
+                throw new ArgumentException("Страница не существует");
+            }
+            else
+            {
+                foreach (var item in page)
+                {
+                    Console.WriteLine(item.Name + " " + item.LastName + " " + item.PhoneNumber);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ HW14/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace HW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook phoneBook = new PhoneBook();
+
+            phoneBook.AddContact(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.AddContact(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.AddContact(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.AddContact(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.AddContact(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.AddContact(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+            phoneBook.AddContact(new Contact("Игорь", "Васильев", 799998000001, "igorVas@example.com"));
+            phoneBook.AddContact(new Contact("Игорь", "Граченко", 799998000001, "igorGrach@example.com"));
+            phoneBook.AddContact(new Contact("Филипп", "Киркоров", 799998000001, "fillKirkorov@example.com"));
+
+
+            while (true)
+            {
+                Console.WriteLine("Введите номер страницы");
+                try
+                {
+                    int str = Convert.ToInt32(Console.ReadLine());
+
+                    try
+                    {
+                        phoneBook.ContactsShowByPages(str);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Вы ввели не числовое значение. Попробуйте ещё раз");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
